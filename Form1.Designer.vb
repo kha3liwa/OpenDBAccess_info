a2656b56fdcbfec4ac9571bcad17e41ee634d266
kha3liwa <kha3liwa@mail.net.sa>
2021-11-25 21:13:16 +0200

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,394 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btsearch = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.bt12 = New System.Windows.Forms.Button()
+        Me.bt4 = New System.Windows.Forms.Button()
+        Me.TitleListBox = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtExt = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(615, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(107, 38)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(327, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tabel Name"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(483, 163)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "???"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Close conn"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkOliveGreen
+        Me.Label2.Location = New System.Drawing.Point(81, 244)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label2.Size = New System.Drawing.Size(108, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "عدد الجداول:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DarkOrange
+        Me.Label3.Location = New System.Drawing.Point(393, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 25)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Indax NO"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.DarkOrange
+        Me.TextBox2.Location = New System.Drawing.Point(452, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(270, 38)
+        Me.TextBox2.TabIndex = 4
+        '
+        'ofd
+        '
+        Me.ofd.FileName = "OpenFileDialog"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 236)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(720, 237)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'btsearch
+        '
+        Me.btsearch.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btsearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btsearch.Location = New System.Drawing.Point(483, 192)
+        Me.btsearch.Name = "btsearch"
+        Me.btsearch.Size = New System.Drawing.Size(103, 41)
+        Me.btsearch.TabIndex = 7
+        Me.btsearch.Text = "search"
+        Me.btsearch.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(592, 192)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(140, 41)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "عرض البيانات"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'bt12
+        '
+        Me.bt12.Location = New System.Drawing.Point(6, 25)
+        Me.bt12.Name = "bt12"
+        Me.bt12.Size = New System.Drawing.Size(173, 32)
+        Me.bt12.TabIndex = 9
+        Me.bt12.Text = "DB_providerACE12"
+        Me.bt12.UseVisualStyleBackColor = True
+        '
+        'bt4
+        '
+        Me.bt4.Location = New System.Drawing.Point(6, 61)
+        Me.bt4.Name = "bt4"
+        Me.bt4.Size = New System.Drawing.Size(173, 30)
+        Me.bt4.TabIndex = 10
+        Me.bt4.Text = "DB_providerJet4"
+        Me.bt4.UseVisualStyleBackColor = True
+        '
+        'TitleListBox
+        '
+        Me.TitleListBox.FormattingEnabled = True
+        Me.TitleListBox.ItemHeight = 20
+        Me.TitleListBox.Location = New System.Drawing.Point(6, 97)
+        Me.TitleListBox.Name = "TitleListBox"
+        Me.TitleListBox.Size = New System.Drawing.Size(173, 144)
+        Me.TitleListBox.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.RosyBrown
+        Me.GroupBox1.Controls.Add(Me.TitleListBox)
+        Me.GroupBox1.Controls.Add(Me.bt12)
+        Me.GroupBox1.Controls.Add(Me.bt4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(738, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.GroupBox1.Size = New System.Drawing.Size(195, 289)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "عرض اسماء الجداول "
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DarkOliveGreen
+        Me.Label4.Location = New System.Drawing.Point(29, 243)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 31)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "label4"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DarkOrange
+        Me.Label5.Location = New System.Drawing.Point(494, 120)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(101, 31)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Label5"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Bisque
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Red
+        Me.Button5.Location = New System.Drawing.Point(744, 326)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(189, 147)
+        Me.Button5.TabIndex = 2
+        Me.Button5.Text = "Exit"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(16, 192)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(461, 38)
+        Me.TextBox3.TabIndex = 4
+        '
+        'txtPath
+        '
+        Me.txtPath.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPath.ForeColor = System.Drawing.Color.Sienna
+        Me.txtPath.Location = New System.Drawing.Point(94, 9)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(352, 20)
+        Me.txtPath.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "File Path:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(13, 31)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "File Name"
+        '
+        'txtName
+        '
+        Me.txtName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtName.ForeColor = System.Drawing.Color.Sienna
+        Me.txtName.Location = New System.Drawing.Point(93, 31)
+        Me.txtName.Name = "txtName"
+        Me.txtName.ReadOnly = True
+        Me.txtName.Size = New System.Drawing.Size(309, 20)
+        Me.txtName.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(13, 56)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(75, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "File Extension:"
+        '
+        'txtExt
+        '
+        Me.txtExt.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtExt.ForeColor = System.Drawing.Color.Sienna
+        Me.txtExt.Location = New System.Drawing.Point(94, 53)
+        Me.txtExt.Name = "txtExt"
+        Me.txtExt.ReadOnly = True
+        Me.txtExt.Size = New System.Drawing.Size(148, 20)
+        Me.txtExt.TabIndex = 17
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(12, 84)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Open conn"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(483, 27)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(126, 17)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "DT.Columns.Count"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Bisque
+        Me.ClientSize = New System.Drawing.Size(945, 485)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtExt)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btsearch)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Open DB Access & Show Tables & some info"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ofd As OpenFileDialog
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btsearch As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents bt12 As Button
+    Friend WithEvents bt4 As Button
+    Friend WithEvents TitleListBox As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtPath As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtExt As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Imports System.IO
+
+
+Public Class Form1
+    'Private ReadOnly dt As String
+    Public conn As New OleDbConnection
+    Public ConnectionString As String
+    Public cmd As New OleDbCommand
+    Public dt As New DataTable
+    'الاتصال بقاعده البيانات 
+
+    Dim providerACE12 As String = "Provider = Microsoft.ACE.OLEDB.12.0;data source=  "
+    Dim providerJet4 As String = "Provider=Microsoft.Jet.OLEDB.4.0;data source=  "
+    Dim ACE_OLEDB12_withpass As String = "Jet OLEDB:Database Password=MyDbPassword "
+    'Dim MyDbPassword As String = InputBox("Enter your password")
+    Dim ds As DataSet
+    Dim da As OleDbDataAdapter
+
+    Private Sub bt4_Click(sender As Object, e As EventArgs) Handles bt4.Click
+
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+
+        ofd.Filter = "Access Databases|*.mdb"
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Settings.conStr = ofd.FileName
+            My.Settings.Save()
+        Else
+            Me.Hide()
+        End If
+        TitleListBox.DataSource = Nothing
+
+        ConnectionString = providerJet4 & My.Settings.conStr & ";"
+        Using conn As New OleDbConnection(ConnectionString)
+
+            conn.Open()
+            Dim dtNames As DataTable = conn.GetSchema("Tables", New String() {Nothing, Nothing, Nothing, "TABLE"})
+
+            TitleListBox.DataSource = dtNames
+            TitleListBox.DisplayMember = "TABLE_NAME"
+
+            Label4.Text = TitleListBox.Items.Count
+
+            txtPath.Text = ofd.FileName
+            txtName.Text = Path.GetDirectoryName(ofd.FileName)
+            txtExt.Text = Path.GetExtension(ofd.FileName)
+
+        End Using
+    End Sub
+
+    Private Sub bt12_Click(sender As Object, e As EventArgs) Handles bt12.Click
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+        ofd.Filter = "Access Databases|*.accdb"
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Settings.conStr = ofd.FileName
+            My.Settings.Save()
+        Else
+            End
+        End If
+        TitleListBox.DataSource = Nothing
+
+        ConnectionString = providerACE12 & My.Settings.conStr & ";"
+
+        Using conn As New OleDbConnection(ConnectionString)
+
+            conn.Open()
+            Dim dtNames As DataTable = conn.GetSchema("Tables", New String() {Nothing, Nothing, Nothing, "TABLE"})
+
+            TitleListBox.DataSource = dtNames
+            TitleListBox.DisplayMember = "TABLE_NAME"
+
+            Label4.Text = TitleListBox.Items.Count
+
+            txtPath.Text = ofd.FileName
+            txtName.Text = Path.GetDirectoryName(ofd.FileName)
+            txtExt.Text = Path.GetExtension(ofd.FileName)
+            conn.Close()
+
+        End Using
+    End Sub
+
+    Private Sub TitleListBox_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As EventArgs) Handles TitleListBox.SelectedIndexChanged
+
+        TextBox2.Text = TitleListBox.GetItemText(TitleListBox.SelectedItem)
+        Label5.Text = TitleListBox.SelectedIndex
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+        TextBox1.Clear()
+        TextBox1.Refresh()
+        conn.Close()
+        dt.Clear()
+        ''columns count
+        'Dim name(dt.Columns.Count) As String
+        'Dim i As Integer = 0
+        'For Each column As DataColumn In dt.Columns
+        '    name(i) = column.ColumnName
+        '    i += 1
+        '    TextBox1.Text = i
+        '    DataGridView1.Refresh()
+        '    'i = 0
+        'Next
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+        Dim conn As New System.Data.OleDb.OleDbConnection(ConnectionString)
+        Try
+            conn.Close()
+            DataGridView1.DataSource = Nothing
+            DataGridView1.Refresh()
+            dt.Clear()
+            cmd = New OleDbCommand("SELECT * FROM " + TextBox2.Text + "", conn)
+            conn.Open()
+            dt.Load(cmd.ExecuteReader)
+            conn.Close()
+            'DataGridView1.Refresh()
+            DataGridView1.DataSource = dt
+            DataGridView1.Refresh()
+        Catch ex As Exception
+            MsgBox("يجب تحديد قاعدة البيانات " & vbCrLf & " اختر احد الخيارين" & vbCrLf & "DB_providerACE12 " & vbCrLf & "DB_providerJet4 ", Nothing, "تحديد قاعدة البيانات")
+
+        End Try
+        conn.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conn.Close()
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Refresh()
+        dt.Clear()
+    End Sub
+End Class
